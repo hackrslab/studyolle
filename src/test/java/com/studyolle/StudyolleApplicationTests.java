f98f44a716e1e78ce3f11a471c4dad66446109c5
Keesun Baik <whiteship2000@gmail.com>
2020-02-24 19:36:12 -0800

    project created

---
+++ src/test/java/com/studyolle/StudyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

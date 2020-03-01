411b7f030e10b735711487183346f919782ead5d
Keesun Baik <whiteship2000@gmail.com>
2020-03-01 14:16:01 -0800

    Zone domain and init data

---
+++ src/main/java/com/studyolle/domain/Zone.java
@@ -0,0 +1,27 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Zone {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String localNameOfCity;
+
+    @Column(nullable = true)
+    private String province;
+
+}
---
+++ src/main/java/com/studyolle/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.studyolle.zone;
+
+import com.studyolle.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}

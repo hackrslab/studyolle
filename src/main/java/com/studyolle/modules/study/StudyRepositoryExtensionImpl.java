1dca6ab3bef487b667d050a2e5b1dc6e2018485f
Keesun Baik <whiteship2000@gmail.com>
2020-03-09 23:58:44 -0700

    Solve n+1 select

--- src/main/java/com/studyolle/modules/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/com/studyolle/modules/study/StudyRepositoryExtensionImpl.java
@@ -1,6 +1,9 @@
 package com.studyolle.modules.study;
 
 import com.querydsl.jpa.JPQLQuery;
+import com.studyolle.modules.account.QAccount;
+import com.studyolle.modules.tag.QTag;
+import com.studyolle.modules.zone.QZone;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
@@ -17,7 +20,11 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
         JPQLQuery<Study> query = from(study).where(study.published.isTrue()
                 .and(study.title.containsIgnoreCase(keyword))
                 .or(study.tags.any().title.containsIgnoreCase(keyword))
-                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)));
+                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)))
+                .leftJoin(study.tags, QTag.tag).fetchJoin()
+                .leftJoin(study.zones, QZone.zone).fetchJoin()
+                .leftJoin(study.members, QAccount.account).fetchJoin()
+                .distinct();
         return query.fetch();
     }
 }

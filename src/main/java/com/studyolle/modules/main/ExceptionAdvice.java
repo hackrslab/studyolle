09c986102f3de130b6850092d00500106d444880
Keesun Baik <whiteship2000@gmail.com>
2020-03-11 09:56:03 -0700

    Error handling

---
+++ src/main/java/com/studyolle/modules/main/ExceptionAdvice.java
@@ -0,0 +1,25 @@
+package com.studyolle.modules.main;
+
+import com.studyolle.modules.account.Account;
+import com.studyolle.modules.account.CurrentAccount;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler
+    public String handleRuntimeException(@CurrentAccount Account account, HttpServletRequest req, RuntimeException e) {
+        if (account != null) {
+            log.info("'{}' requested '{}'", account.getNickname(), req.getRequestURI());
+        } else {
+            log.info("requested '{}'", req.getRequestURI());
+        }
+        log.error("bad request", e);
+        return "error";
+    }
+}
